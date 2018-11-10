d6f614e05cd08108e7484b9bff6c58354dee50a8
qwetuo <chewminzhuang@hotmail.com>
2018-11-10 08:23:43 +0800

    clean up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Test from "./Test";
 import NavBar from "./components/NavBar";
 import SearchSegment from "./components/SearchSegment";
 import Map from "./components/Map";
@@ -33,42 +32,20 @@ class App extends Component {
       isAccModalOpen: false,
       isLoggedIn: true,
       user: localStorage.getItem("user")
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <NavBar openModal={this.openModal} isLoggedIn={this.state.isLoggedIn} />
-        <AccountModal isOpen={this.state.isAccModalOpen} closeModal={this.closeModal} handleLogIn={this.handleLogIn}/>
+        <AccountModal
+          isOpen={this.state.isAccModalOpen}
+          closeModal={this.closeModal}
+          handleLogIn={this.handleLogIn}
+        />
         <Map />
         <SearchSegment />
-        <div className="main-container">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi
-          iusto voluptatibus illo similique laborum, facilis praesentium enim,
-          molestiae, quisquam sint temporibus? Pariatur, vero. Officia voluptate
-          fuga voluptatem, veritatis sequi asperiores. Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Commodi iusto voluptatibus illo
-          similique laborum, facilis praesentium enim, molestiae, quisquam sint
-          temporibus? Pariatur, vero. Officia voluptate fuga voluptatem,
-          veritatis sequi asperiores. Lorem ipsum dolor sit amet, consectetur
-          adipisicing elit. Commodi iusto voluptatibus illo similique laborum,
-          facilis praesentium enim, molestiae, quisquam sint temporibus?
-          Pariatur, vero. Officia voluptate fuga voluptatem, veritatis sequi
-          asperiores. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          Commodi iusto voluptatibus illo similique laborum, facilis praesentium
-          enim, molestiae, quisquam sint temporibus? Pariatur, vero. Officia
-          voluptate fuga voluptatem, veritatis sequi asperiores. Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Commodi iusto
-          voluptatibus illo similique laborum, facilis praesentium enim,
-          molestiae, quisquam sint temporibus? Pariatur, vero. Officia voluptate
-          fuga voluptatem, veritatis sequi asperiores. Lorem ipsum dolor sit
-          amet, consectetur adipisicing elit. Commodi iusto voluptatibus illo
-          similique laborum, facilis praesentium enim, molestiae, quisquam sint
-          temporibus? Pariatur, vero. Officia voluptate fuga voluptatem,
-          veritatis sequi asperiores.
-        </div>
-        {/* <Test /> */}
       </div>
     );
   }
