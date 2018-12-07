ba84af889dea8261cdb46de618e0c6c00433f33c
qwetuo <chewminzhuang@hotmail.com>
2018-12-07 16:31:30 +0800

    use passport and cookieparser on app

--- server/app.js
+++ server/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const cookieParser = require("cookie-parser")
+const { passport } = require("./config/passport");
 
 const { errorHandler } = require("./middlewares/errorHandlers");
 
 const app = express();
 
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 const staticFiles = express.static(path.join(__dirname, "../client/build"));
 
